ffcd36655b85ebe1f9581b35479409bc44fd4a2e
Emily <115892061+Emjem125@users.noreply.github.com>
2025-05-02 19:17:44 +0100

    Nearly fini

--- Reaction.py
+++ Reaction.py
@@ -39,7 +39,8 @@ loop = 0
 average_time = 0
 reaction_times = []
 
-while True and loop < 5:
+while True:
+    print(f"loop:{loop}")
     display_surface.fill(BLUE)
 
     for event in pg.event.get():
@@ -52,11 +53,30 @@ while True and loop < 5:
                 delay_time = random.randint(2000, 4000)
                 trigger_time = pg.time.get_ticks() + delay_time
                 game_state = "waiting"
+                
             elif game_state == "ready":
                 reaction_time = pg.time.get_ticks() - start_time
                 game_state = "result"
+                reaction_times.append(reaction_time)
+
             elif game_state == "result":
-                game_state = "intro"
+                loop += 1
+                if loop >= 5:
+                    game_state = "finish"
+                else:
+                    game_state = "intro"
+
+            elif game_state == "finish":
+                #This section is to either save the score or let the user play again
+                play_again = False
+                exit_game = False
+                if play_again:
+                    game_state = "intro"
+                    loop = 0
+                    reaction_times.clear()
+
+
+
 
     if game_state == "intro":
         display_surface.fill(BLUE)
@@ -68,7 +88,7 @@ while True and loop < 5:
         display_surface.fill(RED)
         draw_centered_text(display_surface, "...", font_dot, WHITE, y_offset=-200)
         draw_centered_text(display_surface, "Wait for green", font_big, WHITE, y_offset=100)
-        
+
         if pg.time.get_ticks() >= trigger_time:
             display_surface.fill(GREEN)
             draw_centered_text(display_surface, "CLICK NOW!", font_big, WHITE)
@@ -82,18 +102,20 @@ while True and loop < 5:
     elif game_state == "result":
         display_surface.fill(BLUE)
         draw_centered_text(display_surface, f"{reaction_time} ms", font_big, WHITE, y_offset=-20)
-        reaction_times.append(reaction_time)
-        
-        if len(reaction_times) < 5:
-            reaction_times.append(reaction_time)
-            loop = len(reaction_times)
-        
-        if loop == 5:
-            average_time = sum(reaction_times) / len(reaction_times)
-            draw_centered_text(display_surface, "Reaction Time", font_small, WHITE, y_offset=-40)
-            draw_centered_text(display_surface, f"{average_time:.2f} ms", font_big, WHITE, y_offset=80)
-            draw_centered_text(display_surface, "Save your score to see how you compare", font_small, WHITE, y_offset=140)
-        else:
-            draw_centered_text(display_surface, "Click to keep going", font_small, WHITE, y_offset=80)
+
+        if loop < 5:#len(reaction_times) < 5:
+            print(reaction_times)
+
+
+        draw_centered_text(display_surface, "Click to keep going", font_small, WHITE, y_offset=80)
+
+    elif game_state == "finish":
+        display_surface.fill(BLUE)
+        average_time = sum(reaction_times) / len(reaction_times)
+        draw_centered_text(display_surface, "Reaction Time", font_small, WHITE, y_offset=-40)
+        draw_centered_text(display_surface, f"{average_time:.2f} ms", font_big, WHITE, y_offset=80)
+        draw_centered_text(display_surface, "Save your score to see how you compare", font_small, WHITE, y_offset=190)
+
+
     pg.display.update()
     clock.tick(60)
